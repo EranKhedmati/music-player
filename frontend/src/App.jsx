8cab7c0a3395a42b75ce876befea3c11c297b351
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-19 23:05:30 +0330

    install and setupt electron app depenencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/data')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>React is dev</h1>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
